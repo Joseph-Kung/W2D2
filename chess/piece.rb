49501d5bd2578574f608fdf61d31c19187584f18
Joseph-Kung <joseph.kung@hotmail.com>
2018-10-30 09:44:00 -0700

    Create skeleton for piece class and modules for stepping and sliding

--- chess/piece.rb
+++ chess/piece.rb
@@ -1,2 +1,20 @@
 class Piece
+  def initialize(color, type, pos, board)
+    @color = color 
+    @type = type
+    @pos = pos
+    @board = board
+  end
+  
+  def moves(pos)
+    
+  end
+end
+
+module SlidingPiece
+  
+end
+
+module SteppingPiece
+  
 end
